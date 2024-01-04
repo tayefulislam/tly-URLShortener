456d2dd92abd32a6dbcb178d7b5a95cb475597b9
tayefulislam <kazitayefulislam12@gmail.com>
2024-01-04 15:33:55 +0600

    server added

---
+++ shorturl-server/server.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = require("./app");
+const port = process.env.PORT || 5000;
+// const uri = process.env.URI;
+
+// mongoose.connect(uri).then(() => {
+//   console.log("database");
+// });
+
+app.listen(port, () => {
+  console.log("server is run ar port", port);
+});
